c70486e3a950f981cf8c93f98d3852ecbf9e5510
Rinventor <rjrs13741@gmail.com>
2023-05-04 06:54:07 +0300

    Delete ClientGameListener.java

--- ClientGameListener.java
+++
@@ -1,9 +0,0 @@
-package com.rinventor.transportmod.testing;
-
-import net.minecraft.network.PacketListener;
-
-public interface ClientGameListener extends PacketListener {
-
-    void handleCount(PTMSavedDataClientboundPacket message);
-
-}
